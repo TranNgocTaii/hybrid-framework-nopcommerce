0e147cc7144e12de9105e816a0747b258a002408
Tran Ngoc Tai <tranngotai2106@gmail.com>
2023-01-28 21:26:08 +0700

    Multiple Browser + Headless

--- src/actions/commons/BaseTest.java
+++ src/actions/commons/BaseTest.java
@@ -1,4 +1,59 @@
 package actions.commons;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class BaseTest {
+    private WebDriver driver;
+    private String projectPath = System.getProperty("user.dir");
+
+    protected WebDriver getBrowserDriver(String browserName){
+        if (browserName.equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browserName.equals("h_chrome")){
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("window-size=1920x1080");
+            driver = new ChromeDriver(options);
+        } else if (browserName.equals("firefox")){
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else if (browserName.equals("h_firefox")){
+            WebDriverManager.firefoxdriver().setup();
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("--headless");
+            options.addArguments("window-size=1920x1080");
+            driver = new FirefoxDriver(options);
+        } else if (browserName.equals("edge")){
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        } else if (browserName.equals("opera")){
+            WebDriverManager.operadriver().setup();
+            driver = new OperaDriver();
+        } else if (browserName.equals("safari")){
+            WebDriverManager.safaridriver().setup();
+            driver = new SafariDriver();
+        } else if (browserName.equals("coccoc")){
+            WebDriverManager.chromedriver().driverVersion("109.0.5414.74").setup();
+            ChromeOptions options = new ChromeOptions();
+            options.setBinary("/Applications/CocCoc.app");
+            driver = new ChromeDriver(options);
+        } else {
+            throw new RuntimeException("Browser name invalid");
+        }
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        return driver;
+    }
 }
